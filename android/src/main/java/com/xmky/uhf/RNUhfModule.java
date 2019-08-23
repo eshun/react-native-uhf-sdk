26c60b0cdc7b69e059ff958381127e64113d2830
esun <qq78552423>
2019-08-23 16:06:22 +0800

    hello

--- android/src/main/java/com/xmky/uhf/RNUhfModule.java
+++ android/src/main/java/com/xmky/uhf/RNUhfModule.java
@@ -31,6 +31,8 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class RNUhfModule extends ReactContextBaseJavaModule {
   protected static final Logger logger = LoggerFactory
           .getLogger(RNUhfModule.class.getSimpleName());
