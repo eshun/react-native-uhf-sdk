9cd5e3d0f5276c86025263735b4d6fc26c3b660c
esun <qq78552423>
2019-08-27 11:27:33 +0800

    hello

--- android/src/main/java/com/uhf/RNUhfPackage.java
+++ android/src/main/java/com/uhf/RNUhfPackage.java
@@ -17,6 +17,11 @@ public class RNUhfPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNUhfModule(reactContext));
     }
 
+    @Override
+    public boolean canOverrideExistingModule() {
+        return true;
+    }
+
     // Deprecated from RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
